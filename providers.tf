09f8cc13ceb68ae017b5ef36f4e2cd72786a0b02
Yesid <yesidstk316@gmail.com>
2026-06-06 17:49:03 -0500

    Fix access_key reference

--- providers.tf
+++ providers.tf
@@ -16,7 +16,7 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.acces_key
+  access_key = var.access_key
   secret_key = var.secret_key
   default_tags {
     tags = var.tags
